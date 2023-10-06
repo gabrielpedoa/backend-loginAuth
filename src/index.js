55df34e5dd4b26b69df12ba3226dd9f646a796f7
Gabriel Antunes <gabrielpedoa@gmail.com>
2023-10-06 16:15:04 -0300

    auto

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import router from './router.js';
 
 const app = express();
 const port = 3000;
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }))
+app.use(cors({ credentials: true, origin: "*" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(router())
